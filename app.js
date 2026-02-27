0eb8415ac245057afeccd5c61ceb2ff4505ab823
umidjon-userbot <62179002+umidjon-userbot@users.noreply.github.com>
2026-02-27 13:26:15 +0500

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 // PUT YOUR TOKEN & CHAT ID
 // ==========================
 
-const BOT_TOKEN = "8724309567:AAH1GyhzfRBnAVys0fPS9qIyB5kcilW9W00";
+const BOT_TOKEN = "AAH1GyhzfRBnAVys0fPS9qIyB5kcilW9W00";
 const CHAT_ID   = "660086073";
 
 async function getIPInfo(){
