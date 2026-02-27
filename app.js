56d57e65831047cc986c038a1042e24cc7cb008b
umidjon-userbot <62179002+umidjon-userbot@users.noreply.github.com>
2026-02-27 21:01:47 +0500

    Update app.js

--- app.js
+++ app.js
@@ -309,7 +309,31 @@ function downloadPDF(){
 
   doc.save("flashcards_clean.pdf");
 }
+//soatcha
+//
+//
+function startClock(){
+  const clock = document.getElementById("liveClock");
+  if(!clock) return;
 
+  function update(){
+    const now = new Date();
+
+    const hours = String(now.getHours()).padStart(2,"0");
+    const minutes = String(now.getMinutes()).padStart(2,"0");
+    const seconds = String(now.getSeconds()).padStart(2,"0");
+
+    clock.textContent = `${hours}:${minutes}:${seconds}`;
+  }
+
+  update();
+  setInterval(update, 1000);
+}
+
+document.addEventListener("DOMContentLoaded", ()=>{
+  startClock();
+});
+//
 // ================= INIT =================
 document.addEventListener("DOMContentLoaded", async()=>{
   await loadAllHSK();
